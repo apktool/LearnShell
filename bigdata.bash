35e06254d420cdb3f917a140262552ada3ba8287
呃，无名 <apktoolOo@gmail.com>
2021-04-05 15:27:02 +0800

    Create bigdata.bash

---
+++ bigdata.bash
@@ -0,0 +1,92 @@
+#####################################
+#
+# apache zookeeper
+# shell.bash
+#
+#####################################
+
+#!/usr/local/env bash
+
+bin=$(dirname "${BASH_SOURCE-$0}")
+bin=$(cd "$bin" > /dev/null || exit; pwd)
+cd "$bin" || exit
+
+declare current=$(pwd)
+echo ${current}
+cd ${current}
+
+function start() {
+    bin/zkServer.sh start
+}
+
+function stop() {
+    bin/zkServer.sh stop
+}
+
+$@
+
+#####################################
+#
+# apache hadoop
+# shell.bash
+#
+#####################################
+
+#!/usr/local/env bash
+
+bin=$(dirname "${BASH_SOURCE-$0}")
+bin=$(cd "$bin" > /dev/null || exit; pwd)
+cd "$bin" || exit
+
+declare current=$(pwd)
+echo ${current}
+cd ${current}
+
+function init() {
+    bin/hdfs namenode -format
+}
+
+function start() {
+    sbin/start-dfs.sh
+    # sbin/start-yarn.sh
+}
+
+function stop() {
+    sbin/stop-dfs.sh
+    # sbin/stop-yarn.sh
+}
+
+$@
+
+#####################################
+#
+# apache hbase
+# shell.bash
+#
+#####################################
+
+#!/usr/local/env bash
+
+bin=$(dirname "${BASH_SOURCE-$0}")
+bin=$(cd "$bin" > /dev/null || exit; pwd)
+cd "$bin" || exit
+
+declare current=$(pwd)
+echo ${current}
+cd ${current}
+
+function start() {
+    bin/start-hbase.sh
+    # bin/hbase-daemon.sh --config ${current}/conf start master
+    # bin/hbase-daemons.sh --config ${current}/conf --hosts ${current}/conf/backup-masters start master-backup
+    # bin/hbase-daemons.sh --config ${current}/conf --hosts ${current}/conf/regionservers start regionserver
+}
+
+function stop() {
+    bin/stop-hbase.sh
+    # bin/hbase-daemons.sh --config ${current}/conf --hosts ${current}/conf/regionservers stop regionserver
+    # bin/hbase-daemons.sh --config ${current}/conf --hosts ${current}/conf/backup-masters stop master-backup
+    # bin/hbase-daemon.sh --config ${current}/conf stop master
+}
+
+$@
